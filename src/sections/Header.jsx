82792bfcd4919cabbd788be7867c681ac4caab2b
kTz1 <catalin_simon90@yahoo.com>
2024-11-06 12:52:50 +0200

    initial commit

---
+++ src/sections/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 z-50 w-full py-10">
+      <div>
+        <a href="">
+          <img src="/images/xora.svg" alt="logo" width={115} height={55} />
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
