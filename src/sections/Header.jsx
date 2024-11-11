b7f9917b4f18003bba142c2af715512b66d3bec1
kTz1 <catalin_simon90@yahoo.com>
2024-11-11 15:59:01 +0200

    added faq section

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
       spy
       smooth
       activeClass="nav-active"
-      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h-5"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
     >
       {title}
     </LinkScroll>
